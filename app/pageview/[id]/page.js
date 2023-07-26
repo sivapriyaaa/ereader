2d6c74ec8e00af393ee37dfc74c4408bdff896ad
sivapriyaaa <sivapriyaap293@gmail.com>
2023-07-26 12:03:44 +0530

    pageview

---
+++ app/pageview/[id]/page.js
@@ -0,0 +1,8 @@
+export default function Pageview({ params }) {
+  return (
+    <div className="mt-20">
+      <h1>Work in progress</h1>
+      <div>book id : {params.id}</div>
+    </div>
+  );
+}
---
+++ app/pageview/page.js
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Page View</div>;
+};
+
+export default Page;
