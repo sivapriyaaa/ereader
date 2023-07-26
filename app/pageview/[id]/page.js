394d9189087e28abd94dd1cccc387ac69c46d637
AvinashBasavaraju <avinash.basavaraju04@gmail.com>
2023-07-26 16:58:46 +0530

    e-reader integration

--- app/pageview/[id]/page.js
+++ app/pageview/[id]/page.js
@@ -1,8 +1,22 @@
 export default function Pageview({ params }) {
+  const viewer = useRef(null);
+
+  useEffect(() => {
+    import('@pdftron/webviewer').then(() => {
+      WebViewer(
+        {
+          path: '/webviewer/lib',
+          initialDoc: '/files/document.pdf',
+        },
+        viewer.current
+      ).then((instance) => {
+        const { docViewer } = instance;
+      });
+    });
+  }, []);
   return (
     <div className="mt-20">
-      <h1>Work in progress</h1>
-      <div>book id : {params.id}</div>
+     <div className='webviewer' ref={viewer} style={{ height: '100vh' }}></div>
     </div>
   );
 }
